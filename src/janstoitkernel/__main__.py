2aeb93914cdb468afac793ad2a80c819b5b24b5f
jans-code <7jantb@gmx.de>
2023-09-05 13:49:20 +0200

    refactored

--- src/janstoitkernel/__main__.py
+++ src/janstoitkernel/__main__.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# *_* coding: utf-8 *_*
+
+""" Main module """
+
 from ipykernel.kernelapp import IPKernelApp
 from .kernel import janstoitkernel
 IPKernelApp.launch_instance(kernel_class=janstoitkernel)
